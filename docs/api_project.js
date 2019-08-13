56ac7a8bb959d0d1e590778eb7af66dbe5e8d92b
ATechnoHazard <amolele@gmail.com>
2019-08-13 21:40:59 +0530

    Fix apidoc config options and regen docs
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- docs/api_project.js
+++ docs/api_project.js
@@ -2,12 +2,14 @@ define({
   "name": "mailer-microservice",
   "version": "0.1.0",
   "description": "A mailer micoservice intended for integration with project hades, but can also work independently.",
-  "apidoc": "0.3.0",
+  "title": "Mailer microservice docs",
+  "url": "http://localhost/api/v1",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-08-13T05:51:45.414Z",
+    "time": "2019-08-13T16:10:50.299Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
